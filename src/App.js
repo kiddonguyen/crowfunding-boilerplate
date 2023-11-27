72b51596a2a15cc9653846982ce502c4c4a90c66
thanhnguyen <thanhnguyen@mobcec.com>
2023-11-27 08:52:44 +0700

    small fixes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 /* eslint-disable camelcase */
-import { permissions } from "constants/permissions";
-import RequiredAuthPage from "pages/RequiredAuthPage";
-import React, { Suspense, useEffect } from "react";
+import { useEffect } from "react";
 import Modal from "react-modal";
 import { useDispatch, useSelector } from "react-redux";
-import { Route, Routes } from "react-router-dom";
 import { authRefreshToken, authUpdateUser } from "store/auth/auth-slice";
 import { getToken, logOut } from "utils/auth";
-// Sử dụng React.lazy() để chỉ tại một trang cụ thể khi user truy cập nó
-const SignUpPage        = React.lazy(() => import("./pages/SignUpPage"));
-const SignInPage        = React.lazy(() => import("./pages/SignInPage"));
-const DashboardPage     = React.lazy(() => import("./pages/DashboardPage"));
-const CampaignPage      = React.lazy(() => import("./pages/CampaignPage"));
-const StartCampaignPage = React.lazy(() => import("./pages/StartCampaignPage"));
-const CheckoutPage      = React.lazy(() => import("./pages/CheckoutPage"));
-const ShippingPage      = React.lazy(() => import("./pages/ShippingPage"));
-const UnauthorizedPage  = React.lazy(() => import("./pages/UnauthorizedPage"));
-const CampaignView      = React.lazy(() =>
-  import("./modules/campaign/CampaignView")
-);
-const LayoutPayment     = React.lazy(() => import("./layout/LayoutPayment"));
-const LayoutDashboard   = React.lazy(() => import("./layout/LayoutDashboard"));
-
 // eslint-disable-next-line no-unused-vars
 const customStyles = {
   content: {},
@@ -51,31 +33,7 @@ function App() {
     }
   }, [user]);
 
-  return (
-    <Suspense>
-      <Routes>
-        <Route element={<LayoutDashboard></LayoutDashboard>}>
-          <Route index path="/" element={<DashboardPage />} />
-          <Route path="/campaign" element={<CampaignPage />} />
-          <Route
-            element={
-              <RequiredAuthPage allowPermissions={[permissions.campaign.CREATE_CAMPAIGN]}></RequiredAuthPage>
-            }
-          >
-            <Route path="/start-campaign" element={<StartCampaignPage />} />
-          </Route>
-          <Route path="/campaign/:slug" element={<CampaignView />} />
-          <Route path="/unauthorized" element={<UnauthorizedPage />} />
-        </Route>
-        <Route element={<LayoutPayment />}>
-          <Route path="/checkout" element={<CheckoutPage />}></Route>
-          <Route path="/shipping" element={<ShippingPage />}></Route>
-        </Route>
-        <Route path="/register" element={<SignUpPage />} />
-        <Route path="/login" element={<SignInPage />} />
-      </Routes>
-    </Suspense>
-  );
+  return <></>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,92 @@
-import React from "react";
+import React, { Suspense } from "react";
+
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { store } from "store/configureStore";
 import App from "./App";
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
-import { store } from "./store/configureStore";
-const container = document.getElementById("root");
-const root      = createRoot(container);
-
-root.render(
-  <Provider store={store}>
-    {/* BrowserRouter sử dụng HTML5 History API để thay đổi URL và hiển thị các trang
-    khác nhau trong ứng dụng của bạn mà không cần tải lại toàn bộ trang */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+// Sử dụng React.lazy() để chỉ tại một trang cụ thể khi user truy cập nó
+const SignUpPage        = React.lazy(() => import("./pages/SignUpPage"));
+const SignInPage        = React.lazy(() => import("./pages/SignInPage"));
+const DashboardPage     = React.lazy(() => import("./pages/DashboardPage"));
+const CampaignPage      = React.lazy(() => import("./pages/CampaignPage"));
+const StartCampaignPage = React.lazy(() => import("./pages/StartCampaignPage"));
+const CheckoutPage      = React.lazy(() => import("./pages/CheckoutPage"));
+const ShippingPage      = React.lazy(() => import("./pages/ShippingPage"));
+const UnauthorizedPage  = React.lazy(() => import("./pages/UnauthorizedPage"));
+const RequiredAuthPage  = React.lazy(() => import("./pages/RequiredAuthPage"));
+const CampaignView      = React.lazy(() =>
+  import("./modules/campaign/CampaignView")
 );
+const LayoutPayment     = React.lazy(() => import("./layout/LayoutPayment"));
+const LayoutDashboard   = React.lazy(() => import("./layout/LayoutDashboard"));
+const container         = document.getElementById("root");
+const router            = createBrowserRouter([
+  {
+    element: <LayoutDashboard></LayoutDashboard>,
+    children: [
+      { path: "/", element: <DashboardPage></DashboardPage> },
+      { path: "/campaign", element: <CampaignPage></CampaignPage> },
+      {
+        element: <RequiredAuthPage></RequiredAuthPage>,
+        children: [
+          {
+            path: "/start-campaign",
+            element: <StartCampaignPage></StartCampaignPage>,
+          },
+        ],
+      },
+      {
+        path: "/campaign/:slug",
+        element: <CampaignView></CampaignView>,
+      },
+      {
+        path: "/unauthorized",
+        element: <UnauthorizedPage></UnauthorizedPage>,
+      },
+    ],
+  },
+  {
+    element: <LayoutPayment></LayoutPayment>,
+    children: [
+      { path: "/checkout", element: <CheckoutPage></CheckoutPage> },
+      { path: "/shipping", element: <ShippingPage></ShippingPage> },
+    ],
+  },
+  {
+    path: "/register",
+    element: <SignUpPage></SignUpPage>,
+  },
+  {
+    path: "/login",
+    element: <SignInPage></SignInPage>,
+  },
+]);
+createRoot(container).render(
+  <>
+    <Provider store={store}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <RouterProvider router={router}>
+          <App />
+        </RouterProvider>
+      </Suspense>
+      <ToastContainer bodyClassName="font-primary text-sm"></ToastContainer>
+    </Provider>
+  </>
+);
+
+// root.render(
+//   <Provider store={store}>
+//     {/* BrowserRouter sử dụng HTML5 History API để thay đổi URL và hiển thị các trang
+//     khác nhau trong ứng dụng của bạn mà không cần tải lại toàn bộ trang */}
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>,
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
